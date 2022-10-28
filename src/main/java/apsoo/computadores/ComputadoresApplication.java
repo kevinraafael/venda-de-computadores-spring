2f0486c4127f0f940bc28e346c9ffb5687f6ca63
Kevin Francelino <kevin.francelino@grupocard.com.br>
2022-10-28 09:50:19 -0400

    feat:project starter pack

---
+++ src/main/java/apsoo/computadores/ComputadoresApplication.java
@@ -0,0 +1,13 @@
+package apsoo.computadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apsoo/computadores/ComputadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package apsoo.computadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
