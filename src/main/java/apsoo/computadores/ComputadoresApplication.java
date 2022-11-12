d9378e91e3774c69ffc49304e8dbd81003a87a37
kevinraafael <kevinfrancelino@gmail.com>
2022-11-11 22:31:43 -0400

    javafx config

--- src/main/java/apsoo/computadores/ComputadoresApplication.java
+++ src/main/java/apsoo/computadores/ComputadoresApplication.java
@@ -1,18 +1,70 @@
 package apsoo.computadores;
 
-import apsoo.computadores.domain.repository.PessoaRepository;
-import apsoo.computadores.domain.service.PessoaService;
-import lombok.RequiredArgsConstructor;
+
+import apsoo.computadores.domain.controller.CarrinhosController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.IOException;
 
 
-
+@Configuration
 @SpringBootApplication
-public class ComputadoresApplication {
+
+public class ComputadoresApplication extends Application {
+
+	private static ConfigurableApplicationContext applicationContext;
+
+	@Override
+	public void init() throws Exception {
+		this.applicationContext = SpringApplication.run(ComputadoresApplication.class);
+	}
+
+	@Override
+	public void stop() throws Exception {
+		applicationContext.close();
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComputadoresApplication.class, args);
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+
+
+
+			try {
+
+
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("CarrinhoDeCompras.fxml"));
+
+				loader.setControllerFactory(ComputadoresApplication.getApplicationContext()::getBean);
+
+				Parent view =  (Parent) loader.load();
+				stage.setScene(new Scene(view));
+
+				//stage.initStyle(StageStyle.UNDECORATED);
+
+				CarrinhosController controller = loader.getController();
+				//controller.attachEvent();
+
+				stage.show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+	}
+
+	public static ConfigurableApplicationContext getApplicationContext() {
+		return applicationContext;
 	}
 }
--- src/main/java/apsoo/computadores/domain/controller/CarrinhosController.java
+++ src/main/java/apsoo/computadores/domain/controller/CarrinhosController.java
@@ -1,18 +1,74 @@
 package apsoo.computadores.domain.controller;
 
 
+import apsoo.computadores.ComputadoresApplication;
 import apsoo.computadores.domain.entity.Pessoa;
 import apsoo.computadores.domain.service.PessoaService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import lombok.RequiredArgsConstructor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.springframework.core.io.Resource;
+import java.io.IOException;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
 
 @RequiredArgsConstructor
 public class CarrinhosController {
     private final PessoaService service;
+    @Value("classpath:/views/CarrinhoDeCompras.fxml")
+    private Resource chartResource;
+    private void attachEvent() {
+        System.out.println("AttackEvent");
+//        loginId.getScene().setOnKeyPressed(event -> {
+//
+//            if(event.getCode() == KeyCode.ENTER) {
+//
+//                if(closeBtn.isFocused()) {
+//                    close();
+//                }
+//
+//                if(loginBtn.isFocused()) {
+//                    login();
+//                }
+//            }
+//        });
+    }
+    public  URL fxmlLocation = getClass().getResource("CarrinhoDeCompras.fxml");
+    public URL fxmlLocationStatic = fxmlLocation;
+    public static void loadView(Stage stage) {
 
-//    public String buscaPessoa(Pessoa p){
-//        if(service.buscaPorId(p.getId())!=null){
-//            return  p.getNome();
-//        }
-//         return  null;
-//    }
+        try {
+
+
+           FXMLLoader loader = new FXMLLoader(CarrinhosController.class.getResource("CarrinhoDeCompras.fxml"));
+
+            loader.setControllerFactory(ComputadoresApplication.getApplicationContext()::getBean);
+
+            Parent view = loader.load();
+            stage.setScene(new Scene(view));
+
+            //stage.initStyle(StageStyle.UNDECORATED);
+
+            CarrinhosController controller = loader.getController();
+            controller.attachEvent();
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
