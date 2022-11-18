530e88529227b0d3bca21e8ad7483219df6d77e3
kevinraafael <kevinfrancelino@gmail.com>
2022-11-18 13:45:57 -0400

    feat:funcao de busca , imagem estatica do pc e conexao controller e fxml

--- src/main/java/apsoo/dao/PessoaDao.java
+++ src/main/java/apsoo/dao/PessoaDao.java
@@ -4,9 +4,11 @@ import apsoo.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PessoaDao extends JpaRepository<Pessoa,UUID > {
+    List<Pessoa> findByCpf(String cpf);
 
 }
